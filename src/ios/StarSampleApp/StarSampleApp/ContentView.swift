46e1b74c2d29a3cbb5b1a1bdc7836c7e1176c844
Bailey Pollard <45129610+Baileypollard@users.noreply.github.com>
2021-11-24 17:20:05 -0500

    initial commit

---
+++ src/ios/StarSampleApp/StarSampleApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  StarSampleApp
+//
+//  Created by Bailey Pollard on 2021-11-24.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
